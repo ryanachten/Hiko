c5086b17c7dce1d255b7ab498714f5a14429c847
Ryan Achten <ryanachten@gmail.com>
2017-09-16 17:55:25 +1200

    Add single.php and revise cotnent.php to handle blog post templates, Add singular.php and content-page.php to handle page templates

--- wp-content/themes/dwp-wordpress-custom/singular.php
+++ wp-content/themes/dwp-wordpress-custom/singular.php
@@ -7,7 +7,7 @@
       <!-- The Loop through posts -->
       <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-        <?php get_template_part( 'template-parts/content' ); ?>
+        <?php get_template_part( 'template-parts/content', 'page' ); ?>
 
       <?php endwhile; else : ?>
 
