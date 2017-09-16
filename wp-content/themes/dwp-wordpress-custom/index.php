bb77a8e5f2b0e273c29b635cfd64bbb12c512415
Ryan Achten <ryanachten@gmail.com>
2017-09-16 16:56:50 +1200

    Move index loop content into content (none) includes

--- wp-content/themes/dwp-wordpress-custom/index.php
+++ wp-content/themes/dwp-wordpress-custom/index.php
@@ -6,39 +6,11 @@
 
       <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-          <header class="entry-header">
-
-             <?php the_title( '<h1>', '</h1>'); ?>
-
-          </header>
-
-          <div class="entry-content">
-
-            <?php the_content(); ?>
-
-          </div>
-
-        </article>
+        <?php get_template_part( 'template-parts/content' ); ?>
 
       <?php endwhile; else : ?>
 
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-
-          <header class="entry-header">
-
-            <h1><?php esc_html_e('404', 'dwpcustomtheme'); ?></h1>
-
-          </header>
-
-          <div class="entry-content">
-
-            <p><?php esc_html_e('Sorry, no content to see here', 'dwpcustomtheme'); ?></p>
-
-          </div>
-
-        </article>
+        <?php get_template_part('template-parts/content', 'none'); ?>
 
       <?php endif; ?>
 
---
+++ wp-content/themes/dwp-wordpress-custom/template-parts/content-none.php
@@ -0,0 +1,15 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+  <header class="entry-header">
+
+    <h1><?php esc_html_e('404', 'dwpcustomtheme'); ?></h1>
+
+  </header>
+
+  <div class="entry-content">
+
+    <p><?php esc_html_e('Sorry, no content to see here', 'dwpcustomtheme'); ?></p>
+
+  </div>
+
+</article>
