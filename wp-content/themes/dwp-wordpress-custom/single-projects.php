42baaf78d9081e2e9fb8f4be517670593f23fac3
Ryan Achten <ryanachten@gmail.com>
2017-09-19 17:29:15 +1200

    Add template single post paages for Projects and Series custom post types

---
+++ wp-content/themes/dwp-wordpress-custom/single-projects.php
@@ -0,0 +1,39 @@
+<!-- Template for custom field type Project pages -->
+<?php get_header(); ?>
+
+  <div id="primary" class="content-area">
+
+    <main id="main" class="site-main" role="main">
+
+      <!-- The Loop through posts -->
+      <?php if( have_posts() ) : while( have_posts() ) : the_post(); ?>
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+          <header class="entry-header">
+
+             <?php the_title( '<h2>','</h2>'); ?>
+             <?php the_post_thumbnail( 'full' ); ?>
+
+          </header>
+
+          <div class="entry-content">
+
+            <?php the_content(); ?>
+
+          </div>
+
+        </article>
+
+
+      <?php endwhile; endif; ?>
+
+      <p>Template: single-projects.php</p>
+
+    </main>
+
+  </div>
+
+  <?php get_sidebar(); ?>
+
+<?php get_footer(); ?>
