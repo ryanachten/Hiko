5cbb0babee752711ad53bca2da992c04494953b7
Ryan Achten <ryanachten@gmail.com>
2017-09-21 12:43:00 +1200

    Move to joints starter theme

--- wp-content/themes/dwp-wordpress-jointswp/page.php
+++ wp-content/themes/dwp-wordpress-jointswp/page.php
@@ -1,23 +1,25 @@
 <?php get_header(); ?>
-	
+
 	<div id="content">
-	
+
 		<div id="inner-content" class="row">
-	
-		    <main id="main" class="large-8 medium-8 columns" role="main">
-				
+
+		    <main id="main" class="large-8 medium-8 small-centered columns" role="main">
+
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
-			    
-			    <?php endwhile; endif; ?>							
-			    					
+
+			    <?php endwhile; endif; ?>
+
 			</main> <!-- end #main -->
 
-		    <?php get_sidebar(); ?>
-		    
+		    <?php //get_sidebar(); ?>
+
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
+
+	<p>Template: page.php</p>
 
 <?php get_footer(); ?>
