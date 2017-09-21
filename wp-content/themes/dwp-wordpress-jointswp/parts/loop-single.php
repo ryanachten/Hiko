fb76741003dce2f33f8f12791403c5cccfed138b
Ryan Achten <ryanachten@gmail.com>
2017-09-21 21:12:32 +1200

    Update single styles, Adjust single featured image to section bg-img styles

--- wp-content/themes/dwp-wordpress-jointswp/parts/loop-single.php
+++ wp-content/themes/dwp-wordpress-jointswp/parts/loop-single.php
@@ -1,20 +1,29 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/BlogPosting">
-						
-	<header class="article-header">	
+
+	<header class="article-header">
 		<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 		<?php get_template_part( 'parts/content', 'byline' ); ?>
     </header> <!-- end article header -->
-					
+
     <section class="entry-content" itemprop="articleBody">
-		<?php the_post_thumbnail('full'); ?>
+		<?php //the_post_thumbnail('full'); ?>
+
+		<!-- If post has a thumnail, add to section bg-img -->
+		<?php if( has_post_thumbnail() ): ?>
+		  <section class="articleBody featured-image" itemprop="articleBody" style="background-image: url('<?php
+		    echo esc_url( get_the_post_thumbnail_url($post->ID, 'medium') );
+		  ?>');">
+		  </section> <!-- end article section -->
+		<?php endif; ?>
+
 		<?php the_content(); ?>
 	</section> <!-- end article section -->
-						
+
 	<footer class="article-footer">
 		<?php wp_link_pages( array( 'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'jointswp' ), 'after'  => '</div>' ) ); ?>
-		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>	
+		<p class="tags"><?php the_tags('<span class="tags-title">' . __( 'Tags:', 'jointswp' ) . '</span> ', ', ', ''); ?></p>
 	</footer> <!-- end article footer -->
-						
-	<?php comments_template(); ?>	
-													
+
+	<?php comments_template(); ?>
+
 </article> <!-- end article -->
--- wp-content/themes/dwp-wordpress-jointswp/single.php
+++ wp-content/themes/dwp-wordpress-jointswp/single.php
@@ -1,24 +1,26 @@
 <?php get_header(); ?>
-			
+
 <div id="content">
+
+	<?php //get_sidebar(); ?>
 
 	<div id="inner-content" class="row">
 
-		<main id="main" class="large-8 medium-8 columns" role="main">
-		
+		<main id="main" class="large-8 medium-8 columns small-centered" role="main">
+
 		    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-		
+
 		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
-		    	
+
 		    <?php endwhile; else : ?>
-		
+
 		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
 
 		    <?php endif; ?>
 
 		</main> <!-- end #main -->
 
-		<?php get_sidebar(); ?>
+		<p style="text-align: center;">Template: single.php</p>
 
 	</div> <!-- end #inner-content -->
 
