355f3c5c1535b2539b69441105d9879c90bc5b4c
Ryan Achten <ryanachten@gmail.com>
2017-09-23 20:53:40 +1200

    Add template for about page

---
+++ wp-content/themes/dwp-wordpress-jointswp/page-about.php
@@ -0,0 +1,39 @@
+<?php get_header(); ?>
+
+	<div id="content">
+
+		<div id="inner-content" class="row">
+
+		    <main id="main" class="large-8 medium-8 small-centered columns" role="main">
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+						<article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article" itemscope itemtype="http://schema.org/WebPage">
+
+							<header class="article-header">
+								<h1 class="page-title"><?php the_title(); ?></h1>
+							</header> <!-- end article header -->
+
+							<section class="entry-content" itemprop="articleBody">
+								<?php the_content(); ?>
+							</section> <!-- end article section -->
+
+							<footer class="article-footer">
+
+							</footer> <!-- end article footer -->
+
+						</article> <!-- end article -->
+
+		    <?php endwhile; endif; ?>
+
+			</main> <!-- end #main -->
+
+		    <?php //get_sidebar(); ?>
+
+		</div> <!-- end #inner-content -->
+
+	</div> <!-- end #content -->
+
+	<p style="text-align: center;">Template: page-about.php</p>
+
+<?php get_footer(); ?>
