ad4e08cd104945f0831dea0391e3a22c055c57fa
Ryan Achten <ryanachten@gmail.com>
2017-09-23 13:40:23 +1200

    Add co-authors-plus for multiple author support, Update content-byline part to check for and display multiple authors on pages/archives

--- wp-content/themes/dwp-wordpress-jointswp/parts/content-byline.php
+++ wp-content/themes/dwp-wordpress-jointswp/parts/content-byline.php
@@ -1,4 +1,12 @@
 <p class="byline">
-	<h6 class="subheader"> <?php the_author_posts_link(); ?> </h6>
+	<h6 class="subheader">
+		<?php
+			if( function_exists( 'coauthors_posts_links' ) ){
+				coauthors_posts_links();
+			}else{
+				the_author_posts_link();
+			}			
+		?>
+	</h6>
 	<small> <?php the_time('F j, Y') ?> </small>
 </p>
