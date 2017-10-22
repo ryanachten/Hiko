e27b5ffb4a2c6a8569109157dc5e05b2ef19a2c7
Ryan Achten <ryanachten@gmail.com>
2017-10-23 12:23:40 +1300

    Add reciprocal functionality to check and link Posts and Projects to any Series they are associated with

--- wp-content/themes/dwp-wordpress-jointswp/single.php
+++ wp-content/themes/dwp-wordpress-jointswp/single.php
@@ -12,6 +12,33 @@
 
 		    	<?php get_template_part( 'parts/loop', 'single' ); ?>
 
+					<!-- Check to see if any Series are associated with Post/Project
+					via ACF Relationship reverse query -->
+					<?php $series = get_posts( array(
+						'post_type' => 'series',
+						'meta_query' => array(
+							array(
+								'key' => 'series_parts', //name of the ACF Relantionship
+								'value' => '"' . get_the_ID() . '"', //get ID of post as string
+								'compare' => 'LIKE' // use meta_query LIKE to to match ID against db value in serialized array
+							)
+						)
+					)); ?>
+					<!-- If the Post/Project is associated with a Series
+				link to the Series -->
+					<?php if( $series ): ?>
+						<p><strong>Featured in the following Series:</strong></p>
+						<ul>
+							<?php foreach( $series as $series_post ): ?>
+								<li>
+									<a href="<?php echo get_permalink( $series_post->ID ); ?>">
+										<?php echo get_the_title( $series_post->ID ); ?>
+									</a>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+					<?php endif; ?>
+
 		    <?php endwhile; else : ?>
 
 		   		<?php get_template_part( 'parts/content', 'missing' ); ?>
