b558aa05dea67916660070daf0086c281a392d32
Ryan Achten <ryanachten@gmail.com>
2017-10-25 16:54:35 +1300

    Add basic top-bar nav styles

--- wp-content/themes/dwp-wordpress-jointswp/parts/nav-title-bar.php
+++ wp-content/themes/dwp-wordpress-jointswp/parts/nav-title-bar.php
@@ -10,7 +10,10 @@ $breakpoint = "medium"; ?>
 <div class="top-bar" id="top-bar-menu">
 	<div class="top-bar-left show-for-<?php echo $breakpoint ?>">
 		<ul class="menu">
-			<li><a href="<?php echo home_url(); ?>"><?php bloginfo('name'); ?></a></li>
+			<li><a href="<?php echo home_url(); ?>">
+        <img id="top-bar-menu-logo" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_mainlogo.svg'?>" alt="Home">
+        <span id="top-bar-menu-logotype"><?php bloginfo('name'); ?></span></a>
+      </li>
 		</ul>
 	</div>
 	<div class="top-bar-right">
