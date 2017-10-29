c7c84bce700b19def3c49980c65e4591aa1ef9c2
Ryan Achten <ryanachten@gmail.com>
2017-10-29 13:38:19 +1300

    Add conditional formatting for date and course archives

--- wp-content/themes/dwp-wordpress-jointswp/archive.php
+++ wp-content/themes/dwp-wordpress-jointswp/archive.php
@@ -1,9 +1,22 @@
 <?php get_header(); ?>
 
-	<div id="content" class="row">
+	<div id="content">
 
 		<header class="archive-header medium-10 large-10 small-centered columns" >
-			<h1 class="page-title"><?php the_archive_title();?></h1>
+			<!-- Title formatting for Course archive -->
+			<?php if ( is_tax( 'courses' ) ): ?>
+					<h2>Course:</h2>
+					<h3 class="page-title subheader"><?php single_term_title();?></h3>
+			<!-- Title formatting for Date archive -->
+			<?php elseif ( is_date() ): ?>
+					<h2>Date Archive:</h2>
+					<h3 class="page-title subheader"><?php echo get_the_date( 'F, Y' );?></h3>
+			<!-- Fallback for other stuff -->
+			<?php else: ?>
+				<h2><?php the_archive_title(); ?></h2>
+			<?php endif; ?>
+
+
 		<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
 		</header>
 
@@ -11,7 +24,7 @@
 
 		<hr>
 
-		<div id="inner-content" class="row">
+		<div id="inner-content">
 
 		    <main id="main" class="medium-10 large-10 small-centered columns" role="main">
 
@@ -37,6 +50,6 @@
 	    </div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
-	<p style="text-align: center;">Template: archive.php</p>
+	<p style="clear: both; text-align: center;">Template: archive.php</p>
 
 <?php get_footer(); ?>
