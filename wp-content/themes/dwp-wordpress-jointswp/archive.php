e69f37f75edd91dde4db1007e960bdf74528099b
Ryan Achten <ryanachten@gmail.com>
2017-10-29 14:00:34 +1300

    Add conditional formatting for categories and tags archives

--- wp-content/themes/dwp-wordpress-jointswp/archive.php
+++ wp-content/themes/dwp-wordpress-jointswp/archive.php
@@ -5,12 +5,20 @@
 		<header class="archive-header medium-10 large-10 small-centered columns" >
 			<!-- Title formatting for Course archive -->
 			<?php if ( is_tax( 'courses' ) ): ?>
-					<h2>Course:</h2>
-					<h3 class="page-title subheader"><?php single_term_title();?></h3>
+					<h3 class="page-title">Course:</h3>
+					<h2 class="subheader"><?php single_term_title();?></h2>
 			<!-- Title formatting for Date archive -->
 			<?php elseif ( is_date() ): ?>
-					<h2>Date Archive:</h2>
-					<h3 class="page-title subheader"><?php echo get_the_date( 'F, Y' );?></h3>
+					<h3 class="page-title">Date Archive:</h3>
+					<h2 class="subheader"><?php echo get_the_date( 'F, Y' );?></h2>
+			<!-- Title formatting for Category archive -->
+			<?php elseif ( is_category() ): ?>
+						<h3 class="page-title">Category:</h3>
+						<h2 class="subheader"><?php single_term_title(); ?></h2>
+			<!-- Title formatting for Tag archive -->
+		<?php elseif ( is_tag() ): ?>
+						<h3 class="page-title">Articles Tagged With:</h3>
+						<h2 class="subheader">"<?php single_term_title(); ?>"</h2>
 			<!-- Fallback for other stuff -->
 			<?php else: ?>
 				<h2><?php the_archive_title(); ?></h2>
