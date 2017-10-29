00ae44fcf9b9052ea0ac95f9053a4a123454dfed
Ryan Achten <ryanachten@gmail.com>
2017-10-29 14:10:07 +1300

    Align search archive template  with other archive pages

--- wp-content/themes/dwp-wordpress-jointswp/search.php
+++ wp-content/themes/dwp-wordpress-jointswp/search.php
@@ -1,35 +1,45 @@
 <?php get_header(); ?>
-			
+
 	<div id="content">
 
-		<div id="inner-content" class="row">
-	
-			<main id="main" class="large-8 medium-8 columns first" role="main">
-				<header>
-					<h1 class="archive-title"><?php _e( 'Search Results for:', 'jointswp' ); ?> <?php echo esc_attr(get_search_query()); ?></h1>
-				</header>
+		<header class="archive-header medium-10 large-10 small-centered columns" >
+			<!-- Title formatting for Course archive -->
+			<h3 class="page-title">Search for:</h3>
+			<h2 class="subheader">"<?php echo esc_attr(get_search_query()); ?>"</h2>
 
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			 
+		<?php the_archive_description('<div class="taxonomy-description">', '</div>');?>
+		</header>
+
+		<?php get_sidebar(); ?>
+
+		<hr>
+
+		<div id="inner-content">
+
+		    <main id="main" class="medium-10 large-10 small-centered columns" role="main">
+
+		    	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 					<!-- To see additional archive styles, visit the /parts directory -->
-					<?php get_template_part( 'parts/loop', 'archive' ); ?>
-				    
-				<?php endwhile; ?>	
+					<?php get_template_part( 'parts/loop', 'archive-grid' ); ?>
+
+				<?php endwhile; ?>
 
 					<?php joints_page_navi(); ?>
-					
+
 				<?php else : ?>
-				
+
 					<?php get_template_part( 'parts/content', 'missing' ); ?>
-						
-			    <?php endif; ?>
-	
-		    </main> <!-- end #main -->
-		
-		    <?php get_sidebar(); ?>
-		
-		</div> <!-- end #inner-content -->
+
+				<?php endif; ?>
+
+			</main> <!-- end #main -->
+
+
+
+	    </div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
+	<p style="clear: both; text-align: center;">Template: archive.php</p>
 
 <?php get_footer(); ?>
