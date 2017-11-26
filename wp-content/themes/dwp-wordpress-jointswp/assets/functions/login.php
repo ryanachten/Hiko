28b135930e137c0a64e9b6d350f4b9123b90d38a
Ryan Achten <ryanachten@gmail.com>
2017-11-26 21:17:25 +1300

    Move logotype styles into mixin, Remove dependency from login enque reference

--- wp-content/themes/dwp-wordpress-jointswp/assets/functions/login.php
+++ wp-content/themes/dwp-wordpress-jointswp/assets/functions/login.php
@@ -15,12 +15,23 @@ add_filter('login_headertitle', 'joints_login_title');
 function customise_login_styles(){
 
   // Use main style sheet for login an define via scss
-  wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array('google-fonts'), time(), 'all' );
+  wp_enqueue_style( 'site-css', get_template_directory_uri() . '/assets/css/style.css', array(), time(), 'all' );
 
   ?><style>
     #login h1 a, .login h1 a {
         background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/assets/images/branding-assets/dwp_mainlogo.svg);
+				margin-bottom: 12px;
     }
   </style><?php
 }
 add_action( 'login_enqueue_scripts', 'customise_login_styles' );
+
+
+function the_login_message( $message ) {
+    if ( empty($message) ){
+        return "<p id='login-logotype'>Hiko</p>";
+    } else {
+        return $message;
+    }
+}
+add_filter( 'login_message', 'the_login_message' );
