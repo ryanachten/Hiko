132cfc3bd8fcd5b41c737eeba34800fe7f87390b
Ryan Achten <ryanachten@gmail.com>
2017-11-28 12:12:07 +1300

    Fix break in frontpage byline styles, Update byline date styles

--- wp-content/themes/dwp-wordpress-jointswp/parts/content-byline.php
+++ wp-content/themes/dwp-wordpress-jointswp/parts/content-byline.php
@@ -1,24 +1,22 @@
-<!-- <p > -->
-	<h6 class="byline">
-		<!-- Projects Icon -->
-		<?php if( get_post_type() == 'projects' ): ?>
-			<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_projectlogo_bg.svg'?>" alt="projects page">
-			<!-- Series Icon -->
-		<?php elseif( get_post_type() == 'series' ): ?>
-			<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_serieslogo_bg.svg'?>" alt="series page">
-		<?php elseif( get_post_type() == 'post' ): ?>
-			<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_bloglogo_bg.svg'?>" alt="blog page">
-		<?php endif; ?>
-		<!-- <strong> -->
-		<?php
-			if( function_exists( 'coauthors_posts_links' ) ){
-				coauthors_posts_links( null, ' & ', null, null, true );
-			}else{
-				the_author_posts_link();
-			}	?>
-		<!-- </strong> -->
-		<!-- <br> -->
-	</h6>
-	<p class="byline-divider"><i><?php the_time('F j') ?></i></p>
-	<!-- <small>  </small> -->
-<!-- </p> -->
+<h6 class="byline">
+	<!-- Projects Icon -->
+	<?php if( get_post_type() == 'projects' ): ?>
+		<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_projectlogo_bg.svg'?>" alt="projects page">
+		<!-- Series Icon -->
+	<?php elseif( get_post_type() == 'series' ): ?>
+		<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_serieslogo_bg.svg'?>" alt="series page">
+	<?php elseif( get_post_type() == 'post' ): ?>
+		<img class="byline-typeicon" src="<?php echo get_template_directory_uri() . '/assets/images/branding-assets/dwp_bloglogo_bg.svg'?>" alt="blog page">
+	<?php endif; ?>
+
+	<!-- Author credits -->
+	<?php
+		if( function_exists( 'coauthors_posts_links' ) ){
+			coauthors_posts_links( null, ' & ', null, null, true );
+		}else{
+			the_author_posts_link();
+		}	?>
+</h6>
+
+<!-- Article date -->
+<p class="byline-date"><?php the_time('F j, Y') ?></p>
