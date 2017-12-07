a55584badddcd50f9cffbd8fc4525acd69be83de
Ryan Achten <ryanachten@gmail.com>
2017-12-07 14:30:02 +1300

    Escape and localise archive grid loop

--- wp-content/themes/dwp-wordpress-jointswp/parts/loop-archive-grid.php
+++ wp-content/themes/dwp-wordpress-jointswp/parts/loop-archive-grid.php
@@ -33,14 +33,13 @@ $grid_columns = 4; ?>
 						<?php elseif( get_post_type() == 'post'):  ?>
 							<h4 class="article-title blog-background">
 						<?php endif; ?>
-								<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+								<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php esc_html_e( the_title() ); ?></a>
 							</h4>
 					<?php get_template_part( 'parts/content', 'byline' ); ?>
 				</header> <!-- end article header -->
 
 				<section class="article-body entry-content" itemprop="articleBody">
-					<?php the_excerpt(); ?>
-					<!-- '<button class="tiny">' . __( 'Read more...', 'jointswp' ) . '</button>' -->
+					<?php esc_html_e( the_excerpt() ); ?>
 				</section> <!-- end article section -->
 
 			</article> <!-- end article -->
