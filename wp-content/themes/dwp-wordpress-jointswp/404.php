e589fa5a337a0a739821465bfa53f935f2b3a3d9
Ryan Achten <ryanachten@gmail.com>
2017-12-12 10:48:16 +1300

    Update 404 message

--- wp-content/themes/dwp-wordpress-jointswp/404.php
+++ wp-content/themes/dwp-wordpress-jointswp/404.php
@@ -9,11 +9,11 @@
 				<article id="content-not-found">
 
 					<header class="fourofour-header">
-						<h1 class="text-center"><?php _e( 'Epic 404 - Article Not Found', 'jointswp' ); ?></h1>
+						<h1 class="text-center"><?php _e( 'Epic 404 - Page Not Found', 'jointswp' ); ?></h1>
 					</header> <!-- end article header -->
 
 					<section class="fourofour-content text-center">
-						<h3 class="subheader"><?php _e( 'The article you were looking for was not found, but maybe try looking again!', 'jointswp' ); ?></h3>
+						<h3 class="subheader"><?php _e( 'Sorry! The page you were looking for was not found, how about searching for it below?', 'jointswp' ); ?></h3>
 					</section> <!-- end article section -->
 
 					<section class="search">
